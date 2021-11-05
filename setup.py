f896c29cab5ee1c15fbec785fce91f08e700f30a
pogger-unblocker <93231515+pogger-unblocker@users.noreply.github.com>
2021-11-05 00:33:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-e
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
